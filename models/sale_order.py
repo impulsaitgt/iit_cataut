6c3373fbbc6c721d7969b9328a6f31d77c1b8ce8
Alexander Paiz <impulsaitgt@gmail.com>
2022-03-13 14:45:37 -0600

    Correcta asignacion de autorizaciones

--- models/sale_order.py
+++ models/sale_order.py
@@ -7,6 +7,7 @@ class SaleOrder(models.Model):
     @api.model
     def action_confirm(self):
         autorizado = True
+        mensaje = ''
         for linea in self.order_line:
             if not (linea.sol_autorizado and (linea.sol_precio_autorizado == linea.price_unit)):
                 descuento = linea.product_id.lst_price - linea.price_unit
@@ -17,21 +18,30 @@ class SaleOrder(models.Model):
                                                                        ('monto_final_autorizado', '>=', descuento)],
                                                                        order='monto_inicial_autorizado')
                     if rango:
+                        mensaje = mensaje + 'Producto = ' + linea.product_id.name + ' (supervisores'
+                        for usuario in rango.user_id:
+                            mensaje = mensaje + ' - ' + usuario.name
+                        mensaje = mensaje + ')\n'
                         linea.sol_supervisores = rango.user_id
                         linea.sol_autorizado = False
+                        linea.sol_requiere_autorizacion = True
                         autorizado = False
+                    else:
+                        linea.sol_requiere_autorizacion = False
+                        linea.sol_autorizado = True
 
         if autorizado:
             res = super(SaleOrder, self).action_confirm()
         else:
-            raise ValidationError("Existe productos no autorizados, comuniquese con los supervisores " )
+            raise ValidationError("Existen productos no autorizados, comuniquese con los supervisores, \n" + mensaje)
         return res
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    sol_autorizado = fields.Boolean(string='Autorizado', default=False)
+    sol_autorizado = fields.Boolean(string='Autorizado', default=True)
+    sol_requiere_autorizacion = fields.Boolean(string='Requiere Autorizacion', default=False)
     sol_supervisores = fields.Many2many(string="Superviso(es)", comodel_name="res.users")
     sol_autorizo = fields.Many2one(string="Usuario(s)", comodel_name="res.users")
     sol_fecha_autorizado = fields.Date(string="Fecha de autorizacion")
