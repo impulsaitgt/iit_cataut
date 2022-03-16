5ac6d197c5ed4a02c65c5e383b116e1b700a5206
Alexander Paiz <impulsaitgt@gmail.com>
2022-03-16 17:57:40 -0600

    Agregar funcionalidad de Autorizaciones

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,5 +1,6 @@
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
+from datetime import date
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -8,7 +9,7 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         autorizado = True
         for linea in self.order_line:
-            if not (linea.sol_autorizado and (linea.sol_precio_autorizado == linea.price_unit)):
+            if not (linea.sol_autorizado and (linea.sol_precio_solicitado == linea.price_unit)):
                 descuento = linea.product_id.lst_price - linea.price_unit
                 if descuento > 0:
                     rango = self.env['sol.rango.autorizacion'].search([('company_id', '=', self.env.company.id),
@@ -21,7 +22,10 @@ class SaleOrder(models.Model):
                             'sol_supervisores': rango.user_id,
                             'sol_autorizado': False,
                             'sol_requiere_autorizacion': True,
-                            'sol_precio_autorizado': linea.price_unit
+                            'sol_precio': linea.product_id.lst_price,
+                            'sol_precio_solicitado': linea.price_unit,
+                            'sol_descuento': descuento,
+                            'sol_fecha_autorizado': None
                         }
                         linea_act = self.env['sale.order.line'].search([('id', '=', linea.id)])
                         linea_act.write(vals)
@@ -31,7 +35,10 @@ class SaleOrder(models.Model):
                             'sol_supervisores': None,
                             'sol_autorizado': True,
                             'sol_requiere_autorizacion': False,
-                            'sol_precio_autorizado': linea.price_unit
+                            'sol_precio': linea.product_id.lst_price,
+                            'sol_precio_solicitado': linea.price_unit,
+                            'sol_descuento': descuento,
+                            'sol_fecha_autorizado': date.today()
                         }
                         linea_act = self.env['sale.order.line'].search([('id', '=', linea.id)])
                         linea_act.write(vals)
@@ -54,12 +61,14 @@ class SaleOrderLine(models.Model):
     sol_autorizado = fields.Boolean(string='Autorizado', default=True)
     sol_requiere_autorizacion = fields.Boolean(string='Requiere Autorizacion', default=False)
     sol_supervisores = fields.Many2many(string="Superviso(es)", comodel_name="res.users")
-    sol_autorizo = fields.Many2one(string="Usuario(s)", comodel_name="res.users")
+    sol_autorizo = fields.Many2one(string="Usuario que autorizo", comodel_name="res.users")
     sol_fecha_autorizado = fields.Date(string="Fecha de autorizacion")
-    sol_precio_autorizado = fields.Float(string="Precio", default=1000000)
-
+    sol_precio = fields.Float(string="Precio Lista", default=0)
+    sol_precio_solicitado = fields.Float(string="Precio Solicitado", default=0)
+    sol_descuento = fields.Float(string="Descuento Requerido", default=0)
     sol_etiqueta_autorizacion = fields.Char(string="Autorizado", compute="_etiquetas_")
     sol_etiqueta_requiere_autorizacion = fields.Char(string="Requiere Autorizacion", compute="_etiquetas_")
+    sol_partner_name = fields.Char(string="Cliente", related="order_id.partner_id.name")
 
     def _etiquetas_(self):
         for linea in self:
@@ -72,4 +81,18 @@ class SaleOrderLine(models.Model):
                 linea.sol_etiqueta_requiere_autorizacion = "Requiere Autornizacion"
             else:
                 linea.sol_etiqueta_requiere_autorizacion = "Autorizacion Automatica"
+
+
+    def action_autoriza(self):
+        permite_autorizar = False
+        for usuario in self.sol_supervisores:
+            if self.env.user.id == usuario.id:
+                permite_autorizar = True
+
+        if permite_autorizar:
+            self.sol_autorizado = True
+            self.sol_fecha_autorizado = date.today()
+            self.sol_autorizo = self.env.user
+        else:
+            raise ValidationError('Usted no tiene derechos para autorizar, solo los supervisores asignados!!')
 
